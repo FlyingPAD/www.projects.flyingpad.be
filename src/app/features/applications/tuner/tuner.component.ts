785d8d9b03e65af8449746166a5a2c76572cf984
FlyingPAD <tonyvan@live.fr>
2024-08-01 23:54:15 +0200

    Update Tuner
     - Add Different tunings ....
     - Add Indicator

--- src/app/features/applications/tuner/tuner.component.ts
+++ src/app/features/applications/tuner/tuner.component.ts
@@ -33,10 +33,57 @@ export class TunerComponent implements OnInit, OnDestroy {
     { name: 'E4', frequency: 329.63 }
   ];
 
+  dadaddTuning: Note[] = [
+    { name: 'D2', frequency: 73.42 },
+    { name: 'A2', frequency: 110.00 },
+    { name: 'D3', frequency: 146.83 },
+    { name: 'A3', frequency: 220.00 },
+    { name: 'D4', frequency: 293.66 },
+    { name: 'D4', frequency: 293.66 }
+  ];
+
+  dadadaTuning: Note[] = [
+    { name: 'D2', frequency: 73.42 },
+    { name: 'A2', frequency: 110.00 },
+    { name: 'D3', frequency: 146.83 },
+    { name: 'A3', frequency: 220.00 },
+    { name: 'D4', frequency: 293.66 },
+    { name: 'A4', frequency: 440.00 }
+  ];
+
+  dropCTuning: Note[] = [
+    { name: 'C2', frequency: 65.41 },
+    { name: 'G2', frequency: 97.99 },
+    { name: 'C3', frequency: 130.81 },
+    { name: 'F3', frequency: 174.61 },
+    { name: 'A3', frequency: 220.00 },
+    { name: 'D4', frequency: 293.66 }
+  ];
+
+  dropDTuning: Note[] = [
+    { name: 'D2', frequency: 73.42 },
+    { name: 'A2', frequency: 110.00 },
+    { name: 'D3', frequency: 146.83 },
+    { name: 'G3', frequency: 196.00 },
+    { name: 'B3', frequency: 246.94 },
+    { name: 'E4', frequency: 329.63 }
+  ];
+
+  dadadfsharpTuning: Note[] = [
+    { name: 'D2', frequency: 73.42 },
+    { name: 'A2', frequency: 110.00 },
+    { name: 'D3', frequency: 146.83 },
+    { name: 'A3', frequency: 220.00 },
+    { name: 'D4', frequency: 293.66 },
+    { name: 'F#4', frequency: 369.99 }
+  ];
+
+  currentTuning: Note[] = this.standardTuning;
+
   constructor() {
     this.audioContext = new AudioContext();
     this.analyser = this.audioContext.createAnalyser();
-    this.analyser.fftSize = 2048; // Taille de la FFT
+    this.analyser.fftSize = 2048;
     this.bufferLength = this.analyser.fftSize;
     this.dataArray = new Float32Array(this.bufferLength);
 
@@ -55,7 +102,16 @@ export class TunerComponent implements OnInit, OnDestroy {
       'G3': 'G3.mp3',
       'D3': 'D3.mp3',
       'A2': 'A2.mp3',
-      'E2': 'E2.mp3'
+      'E2': 'E2.mp3',
+      'D2': 'D2.mp3',  // Add files for additional tunings
+      'A3': 'A3.mp3',
+      'D4': 'D4.mp3',
+      'A4': 'A4.mp3',   // Add files for DADADA tuning
+      'C2': 'C2.mp3',   // Add files for Drop C tuning
+      'G2': 'G2.mp3',
+      'C3': 'C3.mp3',
+      'F3': 'F3.mp3',
+      'F#4': 'Fs4.mp3'  // Add files for DADADF# tuning
     };
 
     for (const [note, filename] of Object.entries(noteFilenames)) {
@@ -141,7 +197,7 @@ export class TunerComponent implements OnInit, OnDestroy {
   getClosestStandardTuning(frequency: number): Note {
     let closestNote: Note | null = null;
     let minDifference = Infinity;
-    for (const note of this.standardTuning) {
+    for (const note of this.currentTuning) {  // Use currentTuning
       const difference = Math.abs(frequency - note.frequency);
       if (difference < minDifference) {
         minDifference = difference;
@@ -152,12 +208,40 @@ export class TunerComponent implements OnInit, OnDestroy {
   }
 
   playNote(noteName: string) {
-    if (this.audioBuffers[noteName]) {
+    if (noteName && this.audioBuffers[noteName]) {
       const source = this.audioContext.createBufferSource();
       source.buffer = this.audioBuffers[noteName];
       source.connect(this.audioContext.destination);
       source.start();
     }
+  }
+
+  onTuningChange(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    const tuning = selectElement.value;
+    if (tuning === 'standard') {
+      this.currentTuning = this.standardTuning;
+    } else if (tuning === 'DADADD') {
+      this.currentTuning = this.dadaddTuning;
+    } else if (tuning === 'DADADA') {
+      this.currentTuning = this.dadadaTuning;
+    } else if (tuning === 'dropC') {
+      this.currentTuning = this.dropCTuning;
+    } else if (tuning === 'dropD') {
+      this.currentTuning = this.dropDTuning;
+    } else if (tuning === 'DADADF#') {
+      this.currentTuning = this.dadadfsharpTuning;
+    }
+    this.updatePitch();
+  }
+
+  getIndicatorColor(): string {
+    const maxDifference = 50; // Max difference in Hz for the indicator range
+    const absoluteDifference = Math.abs(this.frequencyDifference);
+    const ratio = Math.min(absoluteDifference / maxDifference, 1);
+    const red = Math.min(255, 510 * ratio);
+    const green = Math.min(255, 510 * (1 - ratio));
+    return `rgb(${red}, ${green}, 0)`;
   }
 
   ngOnDestroy() {
