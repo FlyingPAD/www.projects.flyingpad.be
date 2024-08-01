b51feff772e7f0bda76c5d678620faa8b2e128bf
FlyingPAD <tonyvan@live.fr>
2024-08-01 20:18:37 +0200

    Update Tuner
     - Algorithme de Yin

--- src/app/features/applications/tuner/tuner.component.ts
+++ src/app/features/applications/tuner/tuner.component.ts
@@ -4,7 +4,6 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 export interface Note {
   name: string;
   frequency: number;
-  difference?: number;
 }
 
 @Component({
@@ -18,8 +17,10 @@ export class TunerComponent implements OnInit, OnDestroy {
   private audioContext: AudioContext;
   private analyser: AnalyserNode;
   private dataArray: Float32Array;
+  private bufferLength: number;
   private streamSource: MediaStreamAudioSourceNode | null = null;
-  currentNote: Note | null = null;
+  currentFrequency: number = 0;
+  frequencyDifference: number = 0;
   targetNote: Note | null = null;
   audioBuffers: { [note: string]: AudioBuffer } = {};
 
@@ -35,14 +36,15 @@ export class TunerComponent implements OnInit, OnDestroy {
   constructor() {
     this.audioContext = new AudioContext();
     this.analyser = this.audioContext.createAnalyser();
-    this.analyser.fftSize = 2048;
-    this.dataArray = new Float32Array(this.analyser.frequencyBinCount);
+    this.analyser.fftSize = 2048; // Taille de la FFT
+    this.bufferLength = this.analyser.fftSize;
+    this.dataArray = new Float32Array(this.bufferLength);
 
-    // Charger les fichiers audio
     this.loadAudioFiles();
   }
 
   ngOnInit(): void {
+    console.log('ngOnInit called');
     this.startTuning();
   }
 
@@ -65,7 +67,9 @@ export class TunerComponent implements OnInit, OnDestroy {
 
   async startTuning() {
     try {
+      console.log('Requesting microphone access...');
       const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      console.log('Microphone access granted');
       this.streamSource = this.audioContext.createMediaStreamSource(stream);
       this.streamSource.connect(this.analyser);
       this.updatePitch();
@@ -76,24 +80,62 @@ export class TunerComponent implements OnInit, OnDestroy {
 
   updatePitch() {
     requestAnimationFrame(() => this.updatePitch());
-    this.analyser.getFloatFrequencyData(this.dataArray);
-    const maxIndex = this.dataArray.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
-    const frequency = this.audioContext.sampleRate * maxIndex / this.analyser.fftSize;
-    const noteName = this.frequencyToNoteName(frequency);
-    this.currentNote = { name: noteName, frequency };
-
-    // Trouver la note standard la plus proche
-    const closestNote = this.getClosestStandardTuning(frequency);
-    this.targetNote = closestNote;
-    this.currentNote.difference = frequency - closestNote.frequency;
+    this.analyser.getFloatTimeDomainData(this.dataArray);
+    this.currentFrequency = this.yin(this.dataArray, this.audioContext.sampleRate);
+    if (this.currentFrequency !== -1) {
+      this.targetNote = this.getClosestStandardTuning(this.currentFrequency);
+      this.frequencyDifference = this.currentFrequency - this.targetNote.frequency;
+      console.log('Current Frequency:', this.currentFrequency);
+      console.log('Target Note:', this.targetNote.name);
+      console.log('Frequency Difference:', this.frequencyDifference);
+    }
   }
 
-  frequencyToNoteName(frequency: number): string {
-    const notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
-    const noteNumber = 12 * (Math.log(frequency / 440) / Math.log(2));
-    const noteIndex = Math.round(noteNumber) % 12;
-    const octave = Math.floor(noteNumber / 12) + 4;
-    return notes[noteIndex] + octave;
+  yin(buffer: Float32Array, sampleRate: number): number {
+    const threshold = 0.1;
+    const tau_max = buffer.length / 2;
+    const yinBuffer = new Float32Array(tau_max);
+    let tau, j;
+    let runningSum = 0;
+    
+    yinBuffer[0] = 1;
+
+    for (tau = 1; tau < tau_max; tau++) {
+      yinBuffer[tau] = 0;
+      for (j = 0; j < tau_max; j++) {
+        yinBuffer[tau] += Math.pow(buffer[j] - buffer[j + tau], 2);
+      }
+      runningSum += yinBuffer[tau];
+      yinBuffer[tau] *= tau / runningSum;
+    }
+
+    let minTau = -1;
+    for (tau = 1; tau < tau_max; tau++) {
+      if (yinBuffer[tau] < threshold) {
+        while (tau + 1 < tau_max && yinBuffer[tau + 1] < yinBuffer[tau]) {
+          tau++;
+        }
+        minTau = tau;
+        break;
+      }
+    }
+
+    if (minTau === -1) {
+      return -1;
+    }
+
+    let betterTau;
+    const x1 = yinBuffer[minTau - 1];
+    const x2 = yinBuffer[minTau];
+    const x3 = yinBuffer[minTau + 1];
+
+    if (x1 < x3) {
+      betterTau = minTau - 1 + (x1 - x2) / (x1 - 2 * x2 + x3);
+    } else {
+      betterTau = minTau + (x3 - x2) / (x1 - 2 * x2 + x3);
+    }
+
+    return sampleRate / betterTau;
   }
 
   getClosestStandardTuning(frequency: number): Note {
