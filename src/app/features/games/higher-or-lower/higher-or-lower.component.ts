8170c045566545e0c7c720e5d300c8cdd0406d67
FlyingPAD <99176621+FlyingPAD@users.noreply.github.com>
2024-08-07 13:39:52 +0200

    Update Projects

--- src/app/features/games/higher-or-lower/higher-or-lower.component.ts
+++ src/app/features/games/higher-or-lower/higher-or-lower.component.ts
@@ -8,69 +8,79 @@ import { ButtonTopComponent } from '../../../components/button-top/button-top.co
   standalone: true,
   imports: [CommonModule, FormsModule, ButtonTopComponent],
   templateUrl: './higher-or-lower.component.html',
-  styleUrl: './higher-or-lower.component.scss'
+  styleUrls: ['./higher-or-lower.component.scss']
 })
 export class HigherOrLowerComponent {
-  refLevel : number = 10
-  valueToFind : number = 0
-  userInputValue : number = 0
-  message : string = 'Please select your level'
-  gameStart : boolean = false
-  level : string = '0'
-  moves : number = 0
-  
+  refLevel: number = 10;
+  valueToFind: number = 0;
+  userInputValue: number = 0;
+  message: string = 'Please select your level';
+  gameStart: boolean = false;
+  level: string = '0';
+  levelSelected: boolean = false;
+  moves: number = 0;
+  showTitleScreen: boolean = true;
+
   generateRandom(): void {
-    const min = 1
-    const max = this.refLevel
-    this.valueToFind = Math.floor(Math.random() * (max - min + 1)) + min
+    const min = 1;
+    const max = this.refLevel;
+    this.valueToFind = Math.floor(Math.random() * (max - min + 1)) + min;
   }
+
   process() {
     if (this.userInputValue > this.valueToFind) {
-      this.message = 'Lower'
+      this.message = 'Lower';
     } else if (this.userInputValue < this.valueToFind) {
-      this.message = 'Higher'
+      this.message = 'Higher';
     } else {
-      this.message = 'Correct!'
+      this.message = 'Correct!';
     }
-    this.moves += 1
-    this.checkGameStatus()
+    this.moves += 1;
+    this.checkGameStatus();
   }
 
-  startGame()
-  {
-    if(this.level === '0') {
-      this.message = 'You need to select a level first !'
-      return
+  startGame() {
+    if (this.level === '0') {
+      this.message = 'You need to select a level first!';
+      return;
     }
-    this.generateRandom()
-    this.gameStart = true
-    this.message = 'Let\'s Go !!!'
-    this.moves = 0
+    this.generateRandom();
+    this.gameStart = true;
+    this.message = 'Let\'s Go!!!';
+    this.moves = 0;
   }
 
-  checkGameStatus(){
-    if(this.valueToFind === this.userInputValue)
-    {
-      this.message = 'VICTORY !'
-      this.gameStart = false
+  checkGameStatus() {
+    if (this.valueToFind === this.userInputValue) {
+      this.message = 'VICTORY!';
+      this.gameStart = false;
     }
   }
 
-  selectLevel(level : number):void {
-    if(level === 1)
-    {
-      this.refLevel = 10
-      this.level = '1'
-    }         
-    if(level === 2)
-    {
-      this.refLevel = 100
-      this.level = '2'
-    }
-    if(level === 3)
-    {
+  selectLevel(level: number): void {
+    if (level === 1) {
+      this.refLevel = 10;
+      this.level = '1';
+    } else if (level === 2) {
+      this.refLevel = 100;
+      this.level = '2';
+    } else if (level === 3) {
       this.refLevel = 1000;
-      this.level = '3'
-    } 
+      this.level = '3';
+    }
+    this.levelSelected = true;
+    this.gameStart = false;
+    this.message = `Level ${this.level}`;
+  }
+
+  confirmLevel() {
+    this.levelSelected = false;
+    this.message = `Level ${this.level}. Let's start!`;
+    this.startGame();
+  }
+
+  goToLevelSelection() {
+    this.showTitleScreen = false;
+    this.message = 'Please select your level';
   }
 }
