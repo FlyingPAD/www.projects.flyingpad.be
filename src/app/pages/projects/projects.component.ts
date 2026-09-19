40eed96fe0a724cc05b353e39206494824d7cba3
FlyingPAD <99176621+FlyingPAD@users.noreply.github.com>
2026-09-19 18:30:36 +0200

    refactor: drive projects hub from card data

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { TranslateModule } from '@ngx-translate/core';
-import { BottomBarComponent } from '../../components/bottom-bar/bottom-bar.component';
-import { BottomToggleEntityInfoComponent } from "../../components/bottom-bar-icons/bottom-toggle-entity-info/bottom-icon-about.component";
-import { BottomIconSettingsComponent } from "../../components/bottom-bar-icons/bottom-icon-settings/bottom-icon-settings.component";
-import { BottomIconToTopComponent } from "../../components/bottom-bar-icons/bottom-icon-to-top/bottom-icon-to-top.component";
-import { SpacerComponent } from "../../components/spacer/spacer.component";
+import { RouterLink } from '@angular/router';
+
+type ProjectCard = {
+  index: string;
+  name: string;
+  detail: string;
+  tone: string;
+  route: string;
+};
 
 @Component({
-    selector: 'app-projects',
-    imports: [TranslateModule, RouterModule, BottomBarComponent, BottomToggleEntityInfoComponent, BottomIconSettingsComponent, BottomIconToTopComponent, SpacerComponent],
-    templateUrl: './projects.component.html'
+  selector: 'app-projects',
+  imports: [RouterLink],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-
+  readonly projects: ProjectCard[] = [
+    { index: '02', name: 'Countdowns', detail: 'Dates & timers', tone: 'red', route: '/countdowns' },
+    { index: '03', name: 'Convertors', detail: 'Small utility tools', tone: 'orange', route: '/convertors' },
+    { index: '04', name: 'Virtual Pet', detail: 'State & interaction', tone: 'yellow', route: '/virtual-pet' },
+    { index: '05', name: 'Higher / Lower', detail: 'Guess the number', tone: 'green', route: '/guess-the-right-number' },
+    { index: '06', name: 'Flying Loop', detail: 'Interactive music', tone: 'blue', route: '/flying-loop' },
+    { index: '07', name: 'Dice Roll', detail: 'Turn-based mini game', tone: 'indigo', route: '/dice-roll' },
+    { index: '08', name: 'Leap Year', detail: 'Date logic', tone: 'violet', route: '/leap-year' },
+    { index: '09', name: 'Reaction Time', detail: 'Timing challenge', tone: 'black', route: '/reaction-time' }
+  ];
 }
