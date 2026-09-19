b398d9786ae5ea5a7f5792f6276cd37023f972f8
FlyingPAD <99176621+FlyingPAD@users.noreply.github.com>
2026-09-19 19:44:48 +0200

    refactor: align about page with projects shell

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { ButtonBackComponent } from '../../components/button-back/button-back.component';
-import { BottomBarComponent } from "../../components/bottom-bar/bottom-bar.component";
-import { BottomIconBackComponent } from "../../components/bottom-bar-icons/bottom-icon-back/bottom-icon-back.component";
-import { BottomIconSettingsComponent } from "../../components/bottom-bar-icons/bottom-icon-settings/bottom-icon-settings.component";
-import { BottomToggleEntityInfoComponent } from "../../components/bottom-bar-icons/bottom-toggle-entity-info/bottom-icon-about.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
-    selector: 'app-about',
-    imports: [ButtonBackComponent, BottomBarComponent, BottomIconBackComponent, BottomIconSettingsComponent, BottomToggleEntityInfoComponent],
-    templateUrl: './about.component.html',
-    styleUrl: './about.component.scss'
+  selector: 'app-about',
+  imports: [RouterLink],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.scss'
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
